1c9eb14ce7344afadcec00c36982ec75950df2dd
Klibar <ma8964032@gmail.com>
2026-04-16 22:27:29 +0200

    last update

--- taskFour.dart
+++ taskFour.dart
@@ -1,10 +1,20 @@
 // 4.Write a program to swap two numbers
 void main() {
+  // var x = 7;
+  // var y = 4;
+  // print('befor swap \n x = $x \n y = $y');
+  // x = x ^ y;
+  // y = x ^ y;
+  // x = x ^ y;
+  // print('after swap \n x = $x \n y = $y');
+
+  // another way but simple
   var x = 7;
   var y = 4;
   print('befor swap \n x = $x \n y = $y');
-  x = x ^ y;
-  y = x ^ y;
-  x = x ^ y;
+  var z;
+  z = x;
+  x = y;
+  y = z;
   print('after swap \n x = $x \n y = $y');
 }
